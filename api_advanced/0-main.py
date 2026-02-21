b6fbe600988b89a1e613c83e5e2cbb4facefdb93
tiffany <t.turate@alustudent.com>
2026-02-21 08:36:17 -0800

    Add Reddit API subscriber count function

--- api_advanced/0-main.py
+++ api_advanced/0-main.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+"""
+0-main.py
+Tests the number_of_subscribers function.
+"""
+
 import sys
 number_of_subscribers = __import__('0-subs').number_of_subscribers
 
-if len(sys.argv) < 2:
-    print("Please pass an argument for the subreddit to search.")
-else:
-    print("{:d}".format(number_of_subscribers(sys.argv[1])))
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-0-subs.py
+Function that queries the Reddit API and returns number of subscribers
+for a given subreddit.
 """
 
 import requests
@@ -8,18 +9,33 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Returns the number of subscribers for a given subreddit.
-    If the subreddit is invalid, returns 0.
+    Returns the number of subscribers for a subreddit.
+    If subreddit is invalid, returns 0.
     """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'custom:python:number_of_subscribers:v1.0 (by /u/wintermancer)'}
-    
+
+    headers = {
+        "User-Agent": "linux:subscriber.counter:v1.0 (by u/yourusername)"
+    }
+
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data.get('data', {}).get('subscribers', 0)
-        else:
-            return 0
-    except requests.RequestException:
+        response = requests.get(
+            url,
+            headers=headers,
+            allow_redirects=False
+        )
+    except Exception:
+        return 0
+
+    if response.status_code != 200:
+        return 0
+
+    try:
+        data = response.json().get("data")
+        return data.get("subscribers", 0)
+    except Exception:
         return 0
