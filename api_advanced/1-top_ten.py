61aa4156cd5197e793224bb4fb782a8026e35af1
tiffany <t.turate@alustudent.com>
2026-02-23 07:51:18 -0800

    Add Reddit API top ten hot posts function

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python3
-"""Module that queries Reddit API and prints top 10 hot posts."""
+"""Module that queries Reddit API and prints first 10 hot post titles."""
 
 import requests
 import sys
 
 
 def top_ten(subreddit):
-    """Print titles of the first 10 hot posts for a subreddit."""
+    """Print the titles of the first 10 hot posts for a subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {"User-Agent": "alu-top-ten/1.0"}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-
         if response.status_code != 200:
-            sys.stdout.write("OK")
+            sys.stdout.write("None")
             return
 
         data = response.json()
-        posts = data.get("data", {}).get("children", [])
+        children = data.get("data", {}).get("children", [])
 
-        if not posts:
-            sys.stdout.write("OK")
+        if not children:
+            sys.stdout.write("None")
             return
 
-        for post in posts:
+        for post in children:
             title = post.get("data", {}).get("title")
             if title:
                 print(title)
 
     except Exception:
-        sys.stdout.write("OK")
+        sys.stdout.write("None")
